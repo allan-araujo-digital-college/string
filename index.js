093df7ecc012377f37048f38cee7cb9b11cb96fe
angelolustosa <angelolustosa@gmail.com>
2022-10-03 08:34:43 -0300

    teste string

---
+++ index.js
@@ -0,0 +1,10 @@
+// indexOf
+// split
+// join
+// +
+// substring
+// let te = "0123456789";
+let texto = "Sofia Manzano";
+console.log(texto.substring(0,5) 
++ texto.indexOf("f") + 
+texto.indexOf("o"));
